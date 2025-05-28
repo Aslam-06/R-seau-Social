8135086c88d4ee94894763733aa7d191a0be968f
Mehdi-ahd <nolanpatinde0@gmail.comgit congig --global user.name Mehdi-ahdgit config --global user.email nolanpatinde0@gmail.com>
2025-05-28 23:07:00 +0100

    mise a jour de DataContext

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { DataProvider } from './context/DataContext';
+import { DataProvider } from './context/DataContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
