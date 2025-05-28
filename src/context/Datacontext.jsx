523383fbb8169abde047292a9cc11a99c3f4e4e5
Aslam-06 <aslambouraima@gmail.com>
2025-05-28 23:16:39 +0100

    Update Datacontext.jsx

--- src/context/Datacontext.jsx
+++ src/context/Datacontext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 
-export const DataContext = createContext();
+export const DataContext = createContext(null);
 
 export const DataProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
